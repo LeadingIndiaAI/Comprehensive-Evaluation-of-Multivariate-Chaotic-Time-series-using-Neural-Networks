8e50687b4bbee14064bf7d5dc03cd1b31d66b1bf
amanchills123 <51208890+amanchills123@users.noreply.github.com>
2019-06-18 12:09:00 +0530

    Add files via upload

---
+++ Mackey glass/auto encoder/mackey glass with  mse.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 23:14:14 2019
+
+@author: Aman chills
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset_train = pd.read_csv('mgdata.dat.csv')
+
+training_set = dataset_train.iloc[:1100, 1:2].values
+test_set=dataset_train.iloc[1100:1160, 1:2].values
+test_set=np.reshape(test_set,(1,60))
+test_set= np.reshape(test_set, (1, 60, 1))
+X_train = []
+
+for i in range(59, 1100):
+    X_train.append(training_set[i-59:i+1, 0])
+    
+X_train = np.array(X_train)
+
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
+
+
+model = Sequential()
+model.add(LSTM(100, input_shape=(60,1)))
+model.add(RepeatVector(60))
+model.add(LSTM(100, return_sequences=True))
+model.add(LSTM(100, return_sequences=True))
+model.add(TimeDistributed(Dense(1)))
+model.compile(optimizer='adam', loss='mean_squared_error')
+# fit model
+history=model.fit(X_train, X_train, epochs=60,validation_split=0.03)
+y=model.predict(test_set)
+y=y[0,:,0]
+test_set=test_set[0,:,0]
+plt.plot(test_set, color = 'red', label="real" )
+plt.plot(y, color = 'blue', label="predicted")
+plt.legend()
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
---
+++ Mackey glass/cnn/mackey glass with mse.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 14:42:16 2019
+
+@author: Aman chills
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset = pd.read_csv('mgdata.dat.csv')
+
+training_set = dataset.iloc[:1100, 1:2].values
+X_train=[]
+y_train=[]
+for i in range(20,1090):
+    X_train.append(training_set[i-20:i,0])
+    y_train.append(training_set[i,0])
+X_train=np.asarray(X_train)
+y_train=np.asarray(y_train)
+
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+from keras.models import Sequential
+from keras.layers import Convolution1D
+from keras.layers import MaxPooling1D
+from keras.layers import Flatten
+from keras.layers import Dense
+model = Sequential()
+model.add(Convolution1D(filters=16, kernel_size=3, activation='relu', input_shape=(20,1)))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(20, activation='relu'))
+model.add(Dense(1,activation='linear'))
+model.compile(loss='mean_squared_error', optimizer='adam')
+# fit network
+history=model.fit(X_train,y_train,epochs=60,batch_size=8,validation_split=0.02)
+
+test_set=dataset.iloc[1100:1130, 1:2].values
+X_test=[]
+y_test=[]
+for i in range(20,30):
+    X_test.append(test_set[i-20:i,0])
+    y_test.append(test_set[i,0])
+X_test=np.asarray(X_test)
+y_test=np.asarray(y_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted=model.predict(X_test)
+y_test=np.reshape(y_test,(10,1))
+predicted=np.reshape(predicted,(10,1))
+plt.plot(y_test, color = 'red', label="real" )
+plt.plot(predicted, color = 'blue', label="predicted")
+plt.legend()
+plt.show()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
